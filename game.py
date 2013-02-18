e28955a776ef11d3dbda87a25a903aae90f034df
Matheus Bras <bras.matheus@gmail.com>
2013-02-17 22:23:29 -0300

    Trying to make the dude shoot

--- game.py
+++ game.py
@@ -7,10 +7,13 @@ width, height = 640, 480
 screen = pygame.display.set_mode((width,height))
 keys = [False, False, False, False]
 playerpos = [100,100]
+acc=[0,0]
+arrows=[]
 
 player = pygame.image.load("resources/images/dude.png")
 grass = pygame.image.load("resources/images/grass.png")
 castle = pygame.image.load("resources/images/castle.png")
+arrow = pygame.image.load("resources/images/bullet.png")
 
 while 1:
 	screen.fill(0)
@@ -30,11 +33,30 @@ while 1:
 	playerpos1 = (playerpos[0]-playerrot.get_rect().width/2, playerpos[1]-playerrot.get_rect().height/2)
 	screen.blit(playerrot, playerpos1)
 	pygame.display.flip()
+
+	for bullet in arrows:
+		index=0
+		velx=math.cos(bullet[0])*10
+		vely=math.sin(bullet[0])*10
+		bullet[1]+=velx
+		bullet[2]+=vely
+
+		if bullet[1]<-64 or bullet[1]>640 or bullet[2]<-64 or bullet[2]>480:
+			arrows.pop(index)
+			index+=1
+		for projectile in arrows:
+			arrow1 = pygame.transform.rotate(arrow, 360-projectile[0]*57.29)
+			screen.blit(arrow1, (projectile[1], projectile[2]))
 	
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			exit(0)
+
+		if event.type==pygame.MOUSEBUTTONDOWN:
+			position=pygame.mouse.get_pos()
+			acc[1]+=1
+			arrows.append([math.atan2(position[1]-(playerpos1[1]+32),position[0]-(playerpos1[0]+26)),playerpos1[0]+32,playerpos1[1]+32])
 
 		if event.type == pygame.KEYDOWN:
 			if event.key == K_w:
