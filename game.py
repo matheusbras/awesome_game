c6c5bc624c59f4f42395a9299b151097b5f45fae
Matheus Bras <bras.matheus@gmail.com>
2013-02-17 22:13:09 -0300

    Added movement

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
 import pygame
 from pygame.locals import *
+import math
 
 pygame.init()
 width, height = 640, 480
 screen = pygame.display.set_mode((width,height))
+keys = [False, False, False, False]
+playerpos = [100,100]
 
 player = pygame.image.load("resources/images/dude.png")
 grass = pygame.image.load("resources/images/grass.png")
@@ -17,14 +20,48 @@ while 1:
 			screen.blit(grass,(x*100, y*100))
 
 	screen.blit(castle,(0,30))
-    	screen.blit(castle,(0,135))
-    	screen.blit(castle,(0,240))
-    	screen.blit(castle,(0,345 ))
+	screen.blit(castle,(0,135))
+	screen.blit(castle,(0,240))
+	screen.blit(castle,(0,345 ))
 
-	screen.blit(player, (100, 100))
+	position = pygame.mouse.get_pos()
+	angle = math.atan2(position[1]-(playerpos[1]+32),position[0]-(playerpos[0]+26))
+	playerrot = pygame.transform.rotate(player, 360-angle*57.29)
+	playerpos1 = (playerpos[0]-playerrot.get_rect().width/2, playerpos[1]-playerrot.get_rect().height/2)
+	screen.blit(playerrot, playerpos1)
 	pygame.display.flip()
 	
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			exit(0)
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == K_w:
+				keys[0] = True
+			elif event.key == K_a:
+				keys[1] = True
+			elif event.key == K_s:
+				keys[2] = True
+			elif event.key == K_d:
+				keys[3] = True
+
+		if event.type == pygame.KEYUP:
+			if event.key == K_w:
+				keys[0] = False
+			elif event.key == K_a:
+				keys[1] = False
+			elif event.key == K_s:
+				keys[2] = False
+			elif event.key == K_d:
+				keys[3] = False
+
+		if keys[0]:
+			playerpos[1] -= 5
+		elif keys[2]:
+			playerpos[1] += 5
+		elif keys[1]:
+			playerpos[0] -= 5
+		elif keys[3]:
+			playerpos[0] += 5
+		
